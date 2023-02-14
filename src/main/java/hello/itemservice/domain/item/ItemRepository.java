6b1f6c55140fdfb99df24ee51bfe6556ce6f3e64
pongchul <inchul7511@gmail.com>
2023-02-13 21:30:36 -0800

    add logic !

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -37,4 +37,8 @@ public class ItemRepository {
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
